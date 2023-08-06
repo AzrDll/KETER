c3f8f8de909e5f8d5de09230979015bcf21467f3
Azor <92515100+AzrDll@users.noreply.github.com>
2023-08-06 15:08:44 +0200

    Create README.md

---
+++ README.m
@@ -0,0 +1,67 @@
+Of course
+
+---
+
+# Keter Port Scanner
+
+![Banner Image](https://static.wikia.nocookie.net/scp-containment-is-magic/images/f/fd/Scp_foundation_keter_symbol_warning_by_lycan_therapy-d4v0vp3.png)
+
+A fast and efficient port scanner with an added WhoIs lookup functionality. Built with Python and multi-threading for concurrent port scanning.
+
+---
+
+## Features
+
+- **Multi-threaded Scanning**: Utilizes multi-threading for faster port scanning.
+- **WhoIs Lookup**: Fetches and displays WhoIs details of the target.
+- **Interactive UI**: User-friendly command-line interface with a progress bar.
+- **Comprehensive Results**: Neatly formatted results with a list of open ports.
+
+---
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/AzrDll/KETER.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd keter-port-scanner
+   ```
+
+3. Install the required packages:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+---
+
+## Usage
+
+Run the script:
+
+```bash
+python main_ver_02.py
+```
+
+Follow the on-screen instructions to input the target address and port range.
+
+---
+
+## Contribution
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+---
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+---
+
